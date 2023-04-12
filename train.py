5647f7fc8acfd42fdffde5574db968773f932812
Anastasiya Kuznetsova <missukrof@gmail.com>
2023-04-12 19:00:00 +0300

    final prosti gospadi

--- train.py
+++ train.py
@@ -10,13 +10,13 @@ local_train, local_test = prepare_data_for_train_lfm(
     paths_config=settings.DATA
 )
 
-# X_train, X_test, y_train, y_test = prepare_data_for_train(
-#     paths_config=settings.DATA,
-#     local_test=local_test,
-#     user_features=settings.USERS.USER_FEATURES,
-#     item_features=settings.ITEMS.ITEM_FEATURES,
-#     drop_features=settings.PREPROCESS.DROP_FEATURES
-# )
+X_train, X_test, y_train, y_test = prepare_data_for_train(
+    paths_config=settings.DATA,
+    local_test=local_test,
+    user_features=settings.USERS.USER_FEATURES,
+    item_features=settings.ITEMS.ITEM_FEATURES,
+    drop_features=settings.PREPROCESS.DROP_FEATURES
+)
 
 lfm_model = LFMModel()
 lfm_model.fit(
@@ -25,11 +25,11 @@ lfm_model.fit(
     item_col=settings.ITEMS.ITEM_IDS
 )
 
-# ranker = Ranker()
-# ranker.fit(
-#     X_train=X_train,
-#     y_train=y_train,
-#     X_test=X_test,
-#     y_test=y_test,
-#     categorical_cols=settings.USERS.USER_FEATURES
-# )
+ranker = Ranker()
+ranker.fit(
+    X_train=X_train,
+    y_train=y_train,
+    X_test=X_test,
+    y_test=y_test,
+    categorical_cols=settings.USERS.USER_FEATURES
+)
